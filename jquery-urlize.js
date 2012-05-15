f2486a15b59908f52f646d474218c38d3063baa1
Shoji Matsumoto <shomwoys@gmail.com>
2012-05-15 13:13:13 +0900

    twitter @*, #* urlize

--- jquery-urlize.js
+++ jquery-urlize.js
@@ -14,14 +14,25 @@
 			opts = $.extend({
 				linebreaksbr:true,
 				trunc:[25, 20, false],
-				target:'_blank'
+				target:'_blank',
+				twitter:false
 			}, opts);
 			var target = opts.target ? ' target="'+opts.target+'"' : '';
 			var html = $('<div style="white-space:pre">').text(text).html();
 			html = opts.linebreaksbr ? html.replace(/(\r\n|\r|\n)/g,'<br>') : html;
-			html = html.replace(/\b((https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig,
-				function(match, pos, all){
-					var text = match;
+			// html = html.replace(/\b((https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig,
+			html = html.replace(/([\b\s]?)(@[A-Z0-9_]+|#\S+|(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig,
+				function(all, pre, match){
+					var text = match, h = text.substring(0,1);
+					if (h === '@' || h === '#'){
+						if (opts.twitter){
+							if (h === '@') {
+								return pre+'<a href="https://twitter.com/'+match+'"'+target+'>'+text+'</a>';
+							}
+							return pre+'<a href="https://twitter.com/#!/search/realtime/'+encodeURIComponent(text)+'"'+target+'>'+text+'</a>';
+						}
+						return match;
+					}
 					if (opts.trunc){
 						var h = opts.trunc[0], t = opts.trunc[1], trunc_protocol=opts.trunc[2];
 						if (trunc_protocol) {
@@ -31,7 +42,7 @@
 							text = text.substring(0, h) + '...'+text.substring(text.length-t, text.length);
 						}
 					}
-					return '<a href="'+match+'"'+target+'>'+text+'</a>';
+					return pre+'<a href="'+match+'"'+target+'>'+text+'</a>';
 				}
 			);
 			return html;
